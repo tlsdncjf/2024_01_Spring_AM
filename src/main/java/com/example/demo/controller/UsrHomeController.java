64bd52a23d3640c6ada8bfdf11da9ec086988efe
shinwc147369@gmail.com <shinwc147369@gmail.com>
2024-01-26 17:26:58 +0900

    컨트롤액션

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -6,32 +6,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
-	
-	private int i;
-	
-	@RequestMapping("/usr/home/main")
-	@ResponseBody
-	public String showMain() {
-		return "안녕하세요";
-	}
-	@RequestMapping("/usr/home/main2")
-	@ResponseBody
-	public String showMain2() {
-		return "잘가";
-	}
-	@RequestMapping("/usr/home/main3")
-	@ResponseBody
-	public int showMain3() {
-		int a = 1;
-		int b = 2;
-		return a + b;
-	}
-	@RequestMapping("/usr/home/main4")
-	@ResponseBody
-	public int showMain4() {
-		int i = 0;		
-		return i++;
+
+	private int count;
+
+	public UsrHomeController() {
+		count = 0;
 	}
 
+	@RequestMapping("/usr/home/setCount")
+	@ResponseBody
+	public String setCount(int count) {
+		this.count = count;
+		return "count의 값이 " + count + "(으)로 초기화 되었습니다.";
+	}
+
+	@RequestMapping("/usr/home/getCount")
+	@ResponseBody
+	public int getCount() {
+		return count++;
+	}
 
 }
