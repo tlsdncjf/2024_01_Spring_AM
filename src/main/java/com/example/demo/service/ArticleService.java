8108a069f96c2d0a6d5cab5e96d4a52fc5eac110
shinwc147369@gmail.com <shinwc147369@gmail.com>
2024-01-31 10:27:53 +0900

    작업 29, ResultData에 제네릭 추가, doModify, doDelete 에 적용

--- src/main/java/com/example/demo/service/ArticleService.java
+++ src/main/java/com/example/demo/service/ArticleService.java
@@ -21,7 +21,7 @@ public class ArticleService {
 	}
 
 	// 서비스 메서드
-	public ResultData writeArticle(String title, String body) {
+	public ResultData<Integer> writeArticle(String title, String body) {
 		articleRepository.writeArticle(title, body);
 
 		int id = articleRepository.getLastInsertId();
@@ -29,9 +29,8 @@ public class ArticleService {
 		return ResultData.from("S-1", Ut.f("%d번 글이 생성되었습니다", id), id);
 	}
 
-	public ResultData deleteArticle(int id) {
+	public void deleteArticle(int id) {
 		articleRepository.deleteArticle(id);
-		return ResultData.from("S-1", Ut.f("%d번 글이 삭제되었습니다", id), id);
 	}
 
 	public void modifyArticle(int id, String title, String body) {
